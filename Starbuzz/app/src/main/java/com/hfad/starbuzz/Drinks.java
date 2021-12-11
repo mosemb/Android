a13166211e2bbfc96294de93b54ff50129d69cce
mosemb <mosejava@gmail.com>
2021-12-11 02:17:36 +0100

    drink activity

--- Starbuzz/app/src/main/java/com/hfad/starbuzz/Drinks.java
+++ Starbuzz/app/src/main/java/com/hfad/starbuzz/Drinks.java
@@ -1,4 +1,37 @@
 package com.hfad.starbuzz;
 
 public class Drinks {
+    private String name;
+    private String description;
+    private int imageResourceId;
+
+    private Drinks(String name, String description, int imageResourceId){
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+
+    }
+
+    public static final Drinks[] drinks = {
+            new Drinks("Latte", "A couple of expresso milk with milk", R.drawable.latte),
+            new Drinks("Capucino", "Expresso, hot milk and steamed milk foam", R.drawable.cappuccino),
+            new Drinks("Filter", "Highest quality beans roasted and steamed", R.drawable.filter)
+    };
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public String getName(){
+        return  this.name;
+    }
+
+    public int getImageResourceId(){
+        return this.imageResourceId;
+    }
+
+    public String toString(){
+        return this.name;
+    }
+
 }
--- Starbuzz/app/src/main/java/com/hfad/starbuzz/MainActivity.java
+++ Starbuzz/app/src/main/java/com/hfad/starbuzz/MainActivity.java
@@ -2,13 +2,39 @@ package com.hfad.starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Creating variable
+    //ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if(position==0){
+                    Intent intent = new Intent(MainActivity.this,DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        };
+
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
