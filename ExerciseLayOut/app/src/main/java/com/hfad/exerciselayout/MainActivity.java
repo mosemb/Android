9f81676f807eca498a10738810af71bb8916ba26
mosemb <mosejava@gmail.com>
2021-12-28 04:12:47 +0100

    some stuff

--- ExerciseLayOut/app/src/main/java/com/hfad/exerciselayout/MainActivity.java
+++ ExerciseLayOut/app/src/main/java/com/hfad/exerciselayout/MainActivity.java
@@ -7,7 +7,9 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,97 +23,113 @@ import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (26) Create an EditText variable called mSearchBoxEditText
-    EditText mSearchBoxEditText;
-    // TODO (27) Create a TextView variable called mUrlDisplayTextView
-    TextView mUrlDisplayTextView;
-    // TODO (28) Create a TextView variable called mSearchResultsTextView
-    TextView mSearchResultsTextView;
+    private EditText mSearchBoxEditText;
+
+    private TextView mUrlDisplayTextView;
+
+    private TextView mSearchResultsTextView;
+
+    // TODO (12) Create a variable to store a reference to the error message TextView
+    private TextView mErrorTextView;
+
+    // TODO (24) Create a ProgressBar variable to store a reference to the ProgressBar
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO (29) Use findViewById to get a reference to mSearchBoxEditText
         mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
 
-
-
-        // TODO (30) Use findViewById to get a reference to mUrlDisplayTextView
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
-
-        // TODO (31) Use findViewById to get a reference to mSearchResultsTextView
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_seach_results_json);
 
+        // TODO (13) Get a reference to the error TextView using findViewById
+        mErrorTextView = (TextView) findViewById(R.id.tv_error_message_display);
+
+        // TODO (25) Get a reference to the ProgressBar using findViewById
+        mProgressBar = (ProgressBar) findViewById(R.id.pb_loading_indicator);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        // creating a menu
-        getMenuInflater().inflate(R.menu.main,menu);
-        return true;
-    }
-
-
-
-    public void makeGithubSearchQuery(){
+    /**
+     * This method retrieves the search text from the EditText, constructs the
+     * URL (using {@link NetworkUtils}) for the github repository you'd like to find, displays
+     * that URL in a TextView, and finally fires off an AsyncTask to perform the GET request using
+     * our {@link GithubQueryTask}
+     */
+    private void makeGithubSearchQuery() {
         String githubQuery = mSearchBoxEditText.getText().toString();
-        //NetworkUtils test = new NetworkUtils();
-        URL url = NetworkUtils.buildUrl(githubQuery);
-        mUrlDisplayTextView.setText(url.toString());
+        URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
+        mUrlDisplayTextView.setText(githubSearchUrl.toString());
+        new GithubQueryTask().execute(githubSearchUrl);
+    }
 
-        // TODO (4) Create a new GithubQueryTask and call its execute method, passing in the url to query
-        new GithubQueryTask().execute(url);
+    // TODO (14) Create a method called showJsonDataView to show the data and hide the error
+    public void showJsonDataView(){
+        mSearchResultsTextView.setVisibility(View.VISIBLE);
+        mErrorTextView.setVisibility(View.INVISIBLE);
+    }
 
+    // TODO (15) Create a method called showErrorMessage to show the error and hide the data
+    public void showErrorMessage(){
+        mErrorTextView.setVisibility(View.VISIBLE);
+        mSearchResultsTextView.setVisibility(View.INVISIBLE);
     }
 
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        // making a Toast
-        int id = item.getItemId();
-        if(id==R.id.action_search){
-            // TODO (4) Remove the Toast message when the search menu item is clicked
+    public class GithubQueryTask extends AsyncTask<URL, Void, String> {
 
-            Context context = MainActivity.this;
-            String message = "Click was clicked";
-            Toast.makeText(context,message,Toast.LENGTH_LONG).show();
-            // TODO (5) Call makeGithubSearchQuery when the search menu item is clicked
-            makeGithubSearchQuery();
+        // TODO (26) Override onPreExecute to set the loading indicator to visible
+        @Override
+        protected void onPreExecute(){
+            ProgressBar progressBar = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+            progressBar.setVisibility(View.VISIBLE);
+
         }
 
-        return super.onOptionsItemSelected(item);
-
-    }
-
-    public class GithubQueryTask extends AsyncTask<URL,Void,String> {
-
         @Override
-        protected String doInBackground(URL... urls) {
-            URL url = urls[0];
-            String urlSearchResults = null;
-
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+            String githubSearchResults = null;
             try {
-                urlSearchResults = NetworkUtils.getResponseFromHttpUrl(url);
-
+                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            return urlSearchResults;
+            return githubSearchResults;
         }
 
         @Override
-        protected void onPostExecute(String s){
-            // TODO (3) Override onPostExecute to display the results in the TextView
-            if(s!=null){
-                mSearchResultsTextView.setText(s);
-            }
-
+        protected void onPostExecute(String githubSearchResults) {
+            // TODO (27) As soon as the loading is complete, hide the loading indicator
+            mProgressBar.setVisibility(View.INVISIBLE);
+            if (githubSearchResults != null && !githubSearchResults.equals("")) {
+                // TODO (17) Call showJsonDataView if we have valid, non-null results
+                showJsonDataView();
+                mSearchResultsTextView.setText(githubSearchResults);
+            }else{
+            // TODO (16) Call showErrorMessage if the result is null in onPostExecute
+            showErrorMessage(); }
         }
+
+
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search) {
+            makeGithubSearchQuery();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
